63e96bd2fb0eae0c04abd6e44cce97cfe6d1a070
jasminpartanen <jasmin.partanen@outlook.com>
2022-04-05 09:42:15 +0300

    Background for leaderboard, added Trapezium.swift

---
+++ outdare/Leaderboard/Leaderboard.swift
@@ -0,0 +1,25 @@
+//
+//  Leaderboard.swift
+//  outdare
+//
+//  Created by Jasmin Partanen on 4.4.2022.
+//
+
+import SwiftUI
+
+struct Leaderboard: View {
+    
+    var body: some View {
+        ZStack() {
+            Trapezium()
+            
+            
+        }
+    }
+}
+
+struct Leaderboard_Previews: PreviewProvider {
+    static var previews: some View {
+        Leaderboard()
+    }
+}
